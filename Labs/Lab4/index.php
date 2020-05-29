a983a94610ba7ed69b2150b4a33d0801bc1a09ee
David Leavenworth <dleavenworth3@gmail.com>
2020-05-28 23:06:03 -0700

    Better

--- Labs/Lab4/index.php
+++ Labs/Lab4/index.php
@@ -1,1 +1,11 @@
-<h1>PHP says: <?=echo "Hello World"?><h1>
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>PHP says: <?=echo "Hello World"?><h1>
+  </body>
+</html>
